5e048f333c91a9754bbf14d9e385ca5c68f80f74
ianwoolf <btw515wolf2@gmail.com>
2017-03-03 17:57:14 +0800

    update

--- query/handler.go
+++ query/handler.go
@@ -1,10 +1,45 @@
 package query
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
+
+type AlertData struct {
+	ID       string        `json:"id"`
+	Message  string        `json:"message"`
+	Details  string        `json:"details"`
+	Time     time.Time     `json:"time"`
+	Duration time.Duration `json:"duration"`
+	Level    string        `json:"level"`
+	Data     Result        `json:"data"`
+}
+
+type Result struct {
+	// StatementID is just the statement's position in the query. It's used
+	// to combine statement results if they're being buffered in memory.
+	StatementID int `json:"-"`
+	Series      Rows
+	Messages    []*Message
+	Err         error
+}
+
+type Message struct {
+	Level string `json:"level"`
+	Text  string `json:"text"`
+}
+
+type Rows []*Row
+
+type Row struct {
+	Name    string            `json:"name,omitempty"`
+	Tags    map[string]string `json:"tags,omitempty"`
+	Columns []string          `json:"columns,omitempty"`
+	Values  [][]interface{}   `json:"values,omitempty"`
+}
 
 // @desc get measurement tags from influxdb deps on ns name
 // @router /tags [get]
@@ -20,8 +55,14 @@ func postDataHandler(resp http.ResponseWriter, req *http.Request) {
 	} else {
 		fmt.Println("read body:", string(body))
 	}
+
+	var alertData AlertData
+	if err = json.Unmarshal(body, &alertData); err != nil {
+		fmt.Println("json unmarshal error:", err.Error())
+	}
+	fmt.Printf("%+v", alertData)
 	// just return the origin influxdb rs
 	resp.Header().Add("Content-Type", "application/json")
-	succResp(resp, "OK", "{\"data\":\"data\"}")
+	succResp(resp, "OK", alertData)
 	resp.WriteHeader(200)
 }
